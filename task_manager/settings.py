3d06d68b42b9b9142120ddd8506cee4c4dc97ee1
Jagrut Manish Thakare <114098488+JagrutThakare@users.noreply.github.com>
2025-01-21 13:47:03 +0530

    v13.2.1 - Added Static Config

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qg8*6*h+jhefnn44b!xv(!*s!=11(+90q_wx7buxsv#)+d2s3r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # for production
 
 
 # Application definition
